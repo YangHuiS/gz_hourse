3f4079c16822c4b4bef4ea37b8ae7a91216a6525
YangHuiS <104430713+YangHuiS@users.noreply.github.com>
2022-05-08 11:48:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,6 +159,218 @@ def get_freq(data):
     freq = num[[i for i in num.index if i not in [' ', '，', '。']]]
     return freq
 
+def main():
+    # %% ----------标题----------
+    st.title(':heart:热门城市房价采集及分析:heart:')
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+
+    # %% ----------会话状态----------
+    if 'first_visit' not in st.session_state:
+        st.session_state.first_visit = True
+    else:
+        st.session_state.first_visit = False
+    # 初始化全局配置
+    if st.session_state.first_visit:
+        st.session_state.date_time = datetime.datetime.now() + datetime.timedelta(
+            hours=8)  # Streamlit Cloud的时区是UTC，加8小时即北京时间
+        st.balloons()
+        st.snow()
+
+    st.write('# :star:热门城市数据采集与分析')
+    # ----------参数面板----------
+    st.write('## :dizzy:数据采集')
+    citys = st.multiselect('选择爬取的城市：👇', ['北京', '上海', '广州', '佛山', '杭州', '海口'])
+    years = st.multiselect('选择爬取的年份：👇', [2019, 2020, 2021, 2022])
+    if st.button('开始爬取'):
+        all_data = pd.DataFrame([])
+        for city in citys:
+            for year in years:
+                tmp = get_city_price(city, year)
+                all_data = pd.concat([all_data, tmp], axis=0)
+                time.sleep(1)
+
+        with st.expander('是否查看采集得到的数据？'):
+            with st.form('example form') as f:
+                ag = AgGrid(
+                    all_data,
+                    height=400,
+                    fit_columns_on_grid_load=True,
+                    reload_data=False
+                )
+                st.form_submit_button()
+
+    st.write('## :dizzy:是否查看所有城市的二手房房价数据？')
+    with st.expander('是'):
+        data = pd.read_excel('./city_data/house_price_city.xlsx', index_col=0)
+        data['月份'] = data['月份'].apply(my_str)
+        data2 = pd.pivot_table(data, index='月份', columns='城市', values='二手房均价')
+        st.dataframe(data2)
+
+    st.write('## :dizzy:描述性统计')
+    with st.expander('点击查看'):
+        tmp = data2.describe()
+        st.dataframe(tmp)
+
+    st.write('## :dizzy:数据可视化')
+    with st.expander('点击查看'):
+        line = Line()
+        line.add_xaxis(list(data2.index))
+        for city in name2id.keys():
+            data_city = data2[city]
+            line.add_yaxis(
+                city, y_axis=list(data_city),
+                markpoint_opts=opts.MarkPointOpts(
+                    data=[
+                        opts.MarkPointItem(type_="max", name="最大值"), opts.MarkPointItem(type_="min", name="最小值"),
+                    ]
+                ),
+            )
+        line.set_series_opts(
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        line.set_global_opts(
+            tooltip_opts=opts.TooltipOpts(trigger="axis"),
+            title_opts=opts.TitleOpts(title="热门城市二手房房价数据", subtitle=f"{data2.index[0]} -- {data2.index[-1]}"),
+            toolbox_opts=opts.ToolboxOpts(
+                is_show=True, orient="vertical", pos_left="90%",
+                feature=opts.ToolBoxFeatureOpts(
+                    brush=opts.ToolBoxFeatureBrushOpts(type_='clear'),
+                    data_zoom=opts.ToolBoxFeatureDataZoomOpts(is_show=False),
+                ),
+            ),
+            xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
+            datazoom_opts=[opts.DataZoomOpts(is_realtime=True, start_value=60, end_value=100, )],
+        )
+        components.html(line.render_embed(), width=1200, height=600)
+
+    st.write('## :dizzy:相关性分析')
+    with st.expander('点击查看相关系数'):
+        choice = st.selectbox('选择相关系数：', ['Pearson相关系数', 'Kendall相关系数', 'Spearman相关系数', '协方差'])
+        if choice == '协方差':
+            corr = data2.cov()
+        else:
+            ind = {'Pearson相关系数': 'pearson', 'Kendall相关系数': 'kendall', 'Spearman相关系数': 'spearman'}
+            corr = data2.corr(method=ind[choice])
+        fig = ff.create_annotated_heatmap(z=corr.values, x=list(corr.index), y=list(corr.columns),
+                                          annotation_text=corr.round(2).values)
+        st.plotly_chart(fig, use_container_width=True)
+
+    st.write('# :star:广州二手房价数据采集与分析')
+    st.write('## :dizzy:广州二手房价数据采集')
+    # ----------任务控制----------
+    if st.button('开始爬取广州二手房价'):
+        # 爬取数据
+        gz_data = get_gz_house()
+        with st.form('example form4') as f2:  # form表单
+            ag4 = AgGrid(
+                gz_data,
+                height=400,
+                fit_columns_on_grid_load=True,  # 列过少的时候，设置True。 列过多的时候就不用设置了
+                reload_data=False
+            )
+            st.form_submit_button()  # 在这里点击提交之后，单元格里面的修改部分就可以传到后面了
+
+    gz_data = get_gz_house()
+    gz_data2 = process_gz(gz_data)
+
+    st.write('## :dizzy:广州二手房价词云图')
+    with st.expander('点击查看'):
+        freq = get_freq(gz_data2)
+        from pyecharts.charts import WordCloud
+        wc = WordCloud()
+        wc.add('', ((i, int(j)) for i, j in zip(freq.index, freq.values)), shape=SymbolType.DIAMOND,
+               textstyle_opts=opts.TextStyleOpts(font_family="cursive"), )
+        components.html(wc.render_embed(), width=1200, height=600)
+
+    st.write('## :dizzy:数据分布情况可视化')
+    with st.expander('点击查看'):
+        col1, col2 = st.columns(2)
+        with col1:
+            choice = st.selectbox('1. 选择查看数据分布情况的列：', ['房屋总价', '房屋单价', '房屋套内面积', '楼龄'])
+            chart = st.selectbox('1. 选择查看数据分布的图像类型：', ['直方图', '箱线图'])
+            st.subheader(f'{choice}的{chart}可视化')
+            if chart == '直方图':
+                fig = px.histogram(gz_data2[choice])
+                st.plotly_chart(fig, use_container_width=True)
+            elif chart == '箱线图':
+                fig = px.box(gz_data2[choice])
+                st.plotly_chart(fig, use_container_width=True)
+
+        with col2:
+            choice = st.selectbox('2. 选择查看数据分布情况的列：', ['房屋总价', '房屋单价', '房屋套内面积', '楼龄'])
+            chart = st.selectbox('2. 选择查看数据分布的图像类型：', ['直方图', '箱线图'])
+            st.subheader(f'{choice}的{chart}可视化')
+            if chart == '直方图':
+                fig = px.histogram(gz_data2[choice])
+                st.plotly_chart(fig, use_container_width=True)
+            elif chart == '箱线图':
+                fig = px.box(gz_data2[choice])
+                st.plotly_chart(fig, use_container_width=True)
+
+    st.write('## :dizzy:广州厅数量、房间数量分布情况')
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander('点击查看厅数量分布情况'):
+            st.subheader('厅数量分布情况')
+            st.bar_chart(gz_data2['厅数量'].value_counts())
+
+    with col2:
+        with st.expander('点击查看房间数量分布情况'):
+            st.subheader('房间数量分布情况')
+            st.bar_chart(gz_data2['房间数量'].value_counts())
+
+    st.write('## :dizzy:房屋朝向、装修情况、建筑类型分布情况')
+    with st.expander('点击查看'):
+        choice = st.selectbox('选择需要分析的列', ['房屋朝向', '装修情况', '建筑类型'])
+        st.subheader(f'{choice}分布情况')
+        fig = px.histogram(data_frame=gz_data2, x=choice, )
+        st.plotly_chart(fig, use_container_width=True)
+
+    st.write('## :dizzy:广州二手房价与房龄、面积的关系可视化')
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander('点击查看楼龄与总价的关系'):
+            st.subheader('楼龄与总价的关系散点图')
+            fig = px.scatter(x='楼龄', y='房屋总价', data_frame=gz_data2)
+            st.plotly_chart(fig, use_container_width=True)
+    with col2:
+        with st.expander('点击查看房屋面积和价格的关系'):
+            st.subheader('房屋套内面积与总价的关系散点图')
+            fig = px.scatter(x='房屋套内面积', y='房屋总价', data_frame=gz_data2)
+            st.plotly_chart(fig, use_container_width=True)
+
+    st.write('## :dizzy:广州二手房价与厅数量、房间数量的关系可视化')
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander('点击查看厅数量与总价的关系'):
+            st.subheader('厅数量与总价的关系')
+            fig = px.box(pd.pivot(gz_data2, columns='厅数量', values='房屋总价'))
+            st.plotly_chart(fig, use_container_width=True)
+    with col2:
+        with st.expander('点击查看房间数量和价格的关系'):
+            st.subheader('房间数量和价格的关系')
+            fig = px.box(pd.pivot(gz_data2, columns='房间数量', values='房屋总价'))
+            st.plotly_chart(fig, use_container_width=True)
+
+    st.write('## :dizzy:广州二手房总价与房屋面积、房间数量、厅数量、楼龄的关系')
+    with st.expander('点击查看相关关系'):
+        x = gz_data2[['房屋套内面积', '厅数量', '房间数量', '楼龄']]
+        y = gz_data2['房屋总价']
+        from sklearn.linear_model import LinearRegression
+        model = LinearRegression()
+        model.fit(x, y)
+        st.markdown(
+            f'$$y = {model.coef_[0]:.2f}*x_1 + {model.coef_[1]:.2f}*x_2 + {model.coef_[2]:.2f}*x_3 + {model.coef_[3]:.2f}*x_4 + {model.intercept_:.2f}$$')
+        st.markdown(
+            '''
+            - $x_1$: 房屋套内面积
+            - $x_2$: 厅数量
+            - $x_3$: 房间数量
+            - $x_4$: 楼龄
+            '''
+        )
+
 # %% ----------网页设置----------
 st.set_page_config(
     page_title="热门城市房价采集及分析", page_icon=":rainbow:",
@@ -166,220 +378,21 @@ st.set_page_config(
 )
 
 st.sidebar.title('虚拟仿真实验')
-st.sidebar.button('房地产数据')
-st.sidebar.button('环境数据')
-st.sidebar.button('消费数据')
+
 session_id = get_report_ctx().session_id
 sessions = Server.get_current()._session_info_by_id
 session_ws = sessions[session_id].ws
+
+
+if st.sidebar.button('房地产数据'):
+    main()
+if st.sidebar.button('环境数据'):
+    # st.write('敬请期待')
+    j = plt.imread('qi.jpg')
+    st.image(j)
+if st.sidebar.button('消费数据'):
+    # st.write('敬请期待')
+    j = plt.imread('qi.jpg')
+    st.image(j)
+
 st.sidebar.info(f'当前在线人数：{len(sessions)}')
-
-# %% ----------标题----------
-st.title(':heart:热门城市房价采集及分析:heart:')
-st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<br>', unsafe_allow_html=True)
-
-# %% ----------会话状态----------
-if 'first_visit' not in st.session_state:
-    st.session_state.first_visit = True
-else:
-    st.session_state.first_visit = False
-# 初始化全局配置
-if st.session_state.first_visit:
-    st.session_state.date_time = datetime.datetime.now() + datetime.timedelta(hours=8)  # Streamlit Cloud的时区是UTC，加8小时即北京时间
-    st.balloons()
-    st.snow()
-
-st.write('# :star:热门城市数据采集与分析')
-# ----------参数面板----------
-st.write('## :dizzy:数据采集')
-citys = st.multiselect('选择爬取的城市：👇', ['北京', '上海',  '广州', '佛山', '杭州', '海口'])
-years = st.multiselect('选择爬取的年份：👇', [2019, 2020, 2021, 2022])
-if st.button('开始爬取'):
-    all_data = pd.DataFrame([])
-    for city in citys:
-        for year in years:
-            tmp = get_city_price(city, year)
-            all_data = pd.concat([all_data, tmp], axis=0)
-            time.sleep(1)
-
-    with st.expander('是否查看采集得到的数据？'):
-        with st.form('example form') as f:
-            ag = AgGrid(
-                all_data,
-                height=400,
-                fit_columns_on_grid_load=True,
-                reload_data=False
-            )
-            st.form_submit_button()
-
-st.write('## :dizzy:是否查看所有城市的二手房房价数据？')
-with st.expander('是'):
-    data = pd.read_excel('./city_data/house_price_city.xlsx', index_col=0)
-    data['月份'] = data['月份'].apply(my_str)
-    data2 = pd.pivot_table(data, index='月份', columns='城市', values='二手房均价')
-    st.dataframe(data2)
-
-st.write('## :dizzy:描述性统计')
-with st.expander('点击查看'):
-    tmp = data2.describe()
-    st.dataframe(tmp)
-
-st.write('## :dizzy:数据可视化')
-with st.expander('点击查看'):
-    line = Line()
-    line.add_xaxis(list(data2.index))
-    for city in name2id.keys():
-        data_city = data2[city]
-        line.add_yaxis(
-            city, y_axis=list(data_city),
-            markpoint_opts=opts.MarkPointOpts(
-                data=[
-                    opts.MarkPointItem(type_="max", name="最大值"), opts.MarkPointItem(type_="min", name="最小值"),
-                ]
-            ),
-        )
-    line.set_series_opts(
-        label_opts=opts.LabelOpts(is_show=False),
-    )
-    line.set_global_opts(
-        tooltip_opts=opts.TooltipOpts(trigger="axis"),
-        title_opts=opts.TitleOpts(title="热门城市二手房房价数据", subtitle=f"{data2.index[0]} -- {data2.index[-1]}"),
-        toolbox_opts=opts.ToolboxOpts(
-            is_show=True, orient="vertical", pos_left="90%",
-            feature=opts.ToolBoxFeatureOpts(
-                brush=opts.ToolBoxFeatureBrushOpts(type_='clear'),
-                data_zoom=opts.ToolBoxFeatureDataZoomOpts(is_show=False),
-            ),
-        ),
-        xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
-        datazoom_opts=[opts.DataZoomOpts(is_realtime=True, start_value=60, end_value=100,)],
-    )
-    components.html(line.render_embed(), width=1200, height=600)
-
-st.write('## :dizzy:相关性分析')
-with st.expander('点击查看相关系数'):
-    choice = st.selectbox('选择相关系数：', ['Pearson相关系数', 'Kendall相关系数', 'Spearman相关系数', '协方差'])
-    if choice == '协方差':
-        corr = data2.cov()
-    else:
-        ind = {'Pearson相关系数': 'pearson', 'Kendall相关系数': 'kendall', 'Spearman相关系数': 'spearman'}
-        corr = data2.corr(method=ind[choice])
-    fig = ff.create_annotated_heatmap(z=corr.values, x=list(corr.index), y=list(corr.columns),
-                                      annotation_text=corr.round(2).values)
-    st.plotly_chart(fig, use_container_width=True)
-
-
-st.write('# :star:广州二手房价数据采集与分析')
-st.write('## :dizzy:广州二手房价数据采集')
-# ----------任务控制----------
-if st.button('开始爬取广州二手房价'):
-    # 爬取数据
-    gz_data = get_gz_house()
-    with st.form('example form4') as f2:  # form表单
-        ag4 = AgGrid(
-            gz_data,
-            height=400,
-            fit_columns_on_grid_load=True,  # 列过少的时候，设置True。 列过多的时候就不用设置了
-            reload_data=False
-        )
-        st.form_submit_button()  # 在这里点击提交之后，单元格里面的修改部分就可以传到后面了
-
-gz_data = get_gz_house()
-gz_data2 = process_gz(gz_data)
-
-st.write('## :dizzy:广州二手房价词云图')
-with st.expander('点击查看'):
-    freq = get_freq(gz_data2)
-    from pyecharts.charts import WordCloud
-    wc = WordCloud()
-    wc.add('', ((i, int(j)) for i, j in zip(freq.index, freq.values)), shape=SymbolType.DIAMOND,
-           textstyle_opts=opts.TextStyleOpts(font_family="cursive"),)
-    components.html(wc.render_embed(), width=1200, height=600)
-
-st.write('## :dizzy:数据分布情况可视化')
-with st.expander('点击查看'):
-    col1, col2 = st.columns(2)
-    with col1:
-        choice = st.selectbox('1. 选择查看数据分布情况的列：', ['房屋总价', '房屋单价', '房屋套内面积', '楼龄'])
-        chart = st.selectbox('1. 选择查看数据分布的图像类型：', ['直方图', '箱线图'])
-        st.subheader(f'{choice}的{chart}可视化')
-        if chart == '直方图':
-            fig= px.histogram(gz_data2[choice])
-            st.plotly_chart(fig, use_container_width=True)
-        elif chart == '箱线图':
-            fig= px.box(gz_data2[choice])
-            st.plotly_chart(fig, use_container_width=True)
-
-    with col2:
-        choice = st.selectbox('2. 选择查看数据分布情况的列：', ['房屋总价', '房屋单价', '房屋套内面积', '楼龄'])
-        chart = st.selectbox('2. 选择查看数据分布的图像类型：', ['直方图', '箱线图'])
-        st.subheader(f'{choice}的{chart}可视化')
-        if chart == '直方图':
-            fig= px.histogram(gz_data2[choice])
-            st.plotly_chart(fig, use_container_width=True)
-        elif chart == '箱线图':
-            fig= px.box(gz_data2[choice])
-            st.plotly_chart(fig, use_container_width=True)
-
-st.write('## :dizzy:广州厅数量、房间数量分布情况')
-col1, col2 = st.columns(2)
-with col1:
-    with st.expander('点击查看厅数量分布情况'):
-        st.subheader('厅数量分布情况')
-        st.bar_chart(gz_data2['厅数量'].value_counts())
-
-with col2:
-    with st.expander('点击查看房间数量分布情况'):
-        st.subheader('房间数量分布情况')
-        st.bar_chart(gz_data2['房间数量'].value_counts())
-
-st.write('## :dizzy:房屋朝向、装修情况、建筑类型分布情况')
-with st.expander('点击查看'):
-    choice = st.selectbox('选择需要分析的列', ['房屋朝向', '装修情况', '建筑类型'])
-    st.subheader(f'{choice}分布情况')
-    fig = px.histogram(data_frame=gz_data2, x=choice, )
-    st.plotly_chart(fig, use_container_width=True)
-
-st.write('## :dizzy:广州二手房价与房龄、面积的关系可视化')
-col1, col2 = st.columns(2)
-with col1:
-    with st.expander('点击查看楼龄与总价的关系'):
-        st.subheader('楼龄与总价的关系散点图')
-        fig = px.scatter(x='楼龄', y='房屋总价', data_frame=gz_data2)
-        st.plotly_chart(fig, use_container_width=True)
-with col2:
-    with st.expander('点击查看房屋面积和价格的关系'):
-        st.subheader('房屋套内面积与总价的关系散点图')
-        fig = px.scatter(x='房屋套内面积', y='房屋总价', data_frame=gz_data2)
-        st.plotly_chart(fig, use_container_width=True)
-
-st.write('## :dizzy:广州二手房价与厅数量、房间数量的关系可视化')
-col1, col2 = st.columns(2)
-with col1:
-    with st.expander('点击查看厅数量与总价的关系'):
-        st.subheader('厅数量与总价的关系')
-        fig = px.box(pd.pivot(gz_data2, columns='厅数量', values='房屋总价'))
-        st.plotly_chart(fig, use_container_width=True)
-with col2:
-    with st.expander('点击查看房间数量和价格的关系'):
-        st.subheader('房间数量和价格的关系')
-        fig = px.box(pd.pivot(gz_data2, columns='房间数量', values='房屋总价'))
-        st.plotly_chart(fig, use_container_width=True)
-
-st.write('## :dizzy:广州二手房总价与房屋面积、房间数量、厅数量、楼龄的关系')
-with st.expander('点击查看相关关系'):
-    x = gz_data2[['房屋套内面积', '厅数量', '房间数量', '楼龄']]
-    y = gz_data2['房屋总价']
-    from sklearn.linear_model import LinearRegression
-    model = LinearRegression()
-    model.fit(x, y)
-    st.markdown(f'$$y = {model.coef_[0]:.2f}*x_1 + {model.coef_[1]:.2f}*x_2 + {model.coef_[2]:.2f}*x_3 + {model.coef_[3]:.2f}*x_4 + {model.intercept_:.2f}$$')
-    st.markdown(
-        '''
-        - $x_1$: 房屋套内面积
-        - $x_2$: 厅数量
-        - $x_3$: 房间数量
-        - $x_4$: 楼龄
-        '''
-    )
